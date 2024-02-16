9877c4404901fb64366dc4caaa5fae54a240b42f
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:13:10 +0200

    Add files via upload

---
+++ 8.FinalVariables/src/main/java/com/mycompany/finalvariables/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalvariables;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+       final int myNum = 15;
+       System.out.println(myNum);
+    }
+}
